c09e238786c9ce6c29547a03b2fe1e1a965a1af0
miwd241 <risulislamrony4@gmail.com>
2024-08-23 19:47:38 +0600

    dashboard page

--- authentication/backend/home/home.php
+++ authentication/backend/home/home.php
@@ -9,7 +9,7 @@ $users =mysqli_query($db,$user_query);
      <div class="row">
      <div class="col">
      <div class="page-description">
-            <h1>Dashboard</h1 >
+            <h1>Dashboard page</h1 >
      </div>     
      </div>
 </div> 
--- authentication/backend/logout/logout.php
+++ authentication/backend/logout/logout.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
-session_unset();
 
+
+//header('location:../../authentication/login.php');
+
+session_unset();
 
 header('location:../../authentication/login.php');
 ?>
